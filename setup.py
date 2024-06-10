ba442483cd89c94c63f000fb6e8c4e5b414f52bc
ronilpatil <ronilpatil2000@gmail.com>
2024-06-10 08:52:50 +0530

    setup of local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='test_package',
+    packages=find_packages(where='src'),
+    version='0.0.1',
+    description='creating dummy pyton package',
+    author='ronilpatil',
+    license='MIT',
+)
