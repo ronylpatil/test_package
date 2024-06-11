a2f4c0797b1d7c9c8d0e6adc43466ea425451cc1
ronilpatil <ronilpatil2000@gmail.com>
2024-06-11 17:38:28 +0530

    big fixes & improvements

--- src/test_package/__init__.py
+++ src/test_package/__init__.py
@@ -1,3 +1,4 @@
 # now no need to import like "import test_package.add import addition" directly we can import "from test_package import addition"
+# add full import here and we can use shorthands while actually importing as package
 from test_package.add import addition, subtraction, division
 from test_package.custom_exception import InvalidUserInput
