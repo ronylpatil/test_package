eb41be83265095febaad7b5155dcc25aea69b749
ronilpatil <ronilpatil2000@gmail.com>
2024-06-11 08:42:23 +0530

    adding imports in __init__

---
+++ src/test_package/__init__.py
@@ -0,0 +1,2 @@
+# now no need to import like "import test_package.add import addition" directly we can import "from test_package import addition"
+from test_package.add import addition, subtraction, division
