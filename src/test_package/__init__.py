079ea5a4d5d5d2e23d7d563e56dda4c010de0128
ronilpatil <ronilpatil2000@gmail.com>
2024-06-11 09:12:49 +0530

    enhancement in imports

--- src/test_package/__init__.py
+++ src/test_package/__init__.py
@@ -1,2 +1,3 @@
 # now no need to import like "import test_package.add import addition" directly we can import "from test_package import addition"
 from test_package.add import addition, subtraction, division
+from test_package.custom_exception import InvalidUserInput
