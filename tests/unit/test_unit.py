25edec7c964c0cd99774dcb8ad928e8d662836ea
ronilpatil <ronilpatil2000@gmail.com>
2024-06-11 09:12:11 +0530

    unit tests

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,2 +1,34 @@
-def test_empty() :
-     assert True
+import pytest
+from test_package import addition, subtraction, division
+from test_package import InvalidUserInput
+
+good_input_data = [(2, 1), (6, 2), (9, 7)]
+
+bad_input_sub = [(1, 2), (4, 5)]
+
+bad_input_div = [(3, 0), (0, 0)]
+
+@pytest.mark.parametrize("a, b", good_input_data)
+def test_addition(a, b):
+     assert addition(a, b) == (a + b)
+
+
+@pytest.mark.parametrize("a, b", good_input_data)
+def test_subtraction(a, b):
+     assert subtraction(a, b) == (a - b)
+     
+
+@pytest.mark.parametrize("a, b", good_input_data)
+def test_division(a, b):
+     assert division(a, b) == (a / b)
+
+
+@pytest.mark.parametrize("a, b", bad_input_sub)
+def test_subtraction_failed(a, b):
+    with pytest.raises((InvalidUserInput)):
+        subtraction(a, b)
+
+@pytest.mark.parametrize("a, b", bad_input_div)
+def test_division_failed(a, b) :
+     with pytest.raises((InvalidUserInput)) :
+          division(a, b)
