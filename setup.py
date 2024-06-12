84aea4056d3a2ce5c7ab6c0ce05b858f508e3f92
ronilpatil <ronilpatil2000@gmail.com>
2024-06-12 20:43:42 +0530

    bug fixes & improvements in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name='test_package',
-    packages=find_packages(where='src'),
-    version='0.0.1',
-    description='creating dummy pyton package',
+    name="test_package",
+    packages=find_packages(where="src"),
+    version="0.0.1",
+    description="creating dummy pyton package",
     long_description=long_description,
     long_description_content="text/markdown",
-    author='ronilpatil',
-    license='MIT',
-    package_dir={'': 'src'},
+    author="ronilpatil",
+    license="MIT",
+    package_dir={"": "src"},
     url=f"https://github.com/ronylpatil/test_package",
     project_urls={
         "Bug Tracker": f"https://github.com/ronylpatil/test_package/issues",
