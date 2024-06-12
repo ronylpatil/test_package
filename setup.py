db8814a2ad1cbe3c42efdbda000ab5f007249a22
ronilpatil <ronilpatil2000@gmail.com>
2024-06-12 21:37:25 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
