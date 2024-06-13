ca6e1b993676f5effa38edc663e2792fe647e41b
ronilpatil <ronilpatil2000@gmail.com>
2024-06-13 11:54:36 +0530

    bug fixes & improvements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="dummypackage12624",
-    version="0.0.24",
+    version="0.0.25",
     author="ronilpatil",
     author_email="xyz@gmail.com",
     description=("Dummy python package"),
